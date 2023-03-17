457605746f21b60fcb2783b969c21d519d0c0a40
Ali <aafaq@msn.com>
2023-03-18 04:35:49 +0500

    [WIP] Added Modules, Initial Flow & Structure

---
+++ network/src/main/java/com/aafaq/network/domain/utils/NetworkConstants.kt
@@ -0,0 +1,27 @@
+package com.aafaq.network.domain.utils
+
+sealed class NetworkConstants {
+    object AlAdan {
+        private const val API_VERSION = "v1"
+        const val BASE_URL = "http://api.aladhan.com/${API_VERSION}"
+
+        /**
+         * Refer to https://aladhan.com/prayer-times-api
+         * */
+        object EndPoints{
+            const val GET_TIME_CALENDAR = "/calendar/{year}/{month}"
+            const val GET_TIME_CALENDAR_BY_ADDRESS ="/calendarByAddress/{year}/{month}"
+            const val GET_TIME_CALENDAR_BY_CITY = "/calendarByCity/{year}/{month}"
+            const val GET_TIME_HIJRI_CALENDAR_BY_ADDRESS = "/hijriCalendarByAddress/{year}/{month}"
+            const val GET_TIME_HIJRI_CALENDAR_BY_CITY = "/hijriCalendarByCity/{year}/{month}"
+            const val GET_PRAYER_TIME_CALCULATION_METHODS = "/methods"
+            const val GET_PRAYER_TIMING = "/timings/{date}"
+            const val GET_PRAYER_TIMING_BY_ADDRESS = "/timingsByAddress/{date}"
+            const val GET_PRAYER_TIMING_BY_CITY = "/timingsByCity/{date}"
+        }
+
+        /**
+         * TODO - add methods here
+         * */
+    }
+}
