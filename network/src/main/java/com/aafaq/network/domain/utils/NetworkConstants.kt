38492009c0c000e9dfb7e7e512a49ec4779c3acb
Ali <aafaq@msn.com>
2023-03-22 01:11:23 +0500

    [WIP] Added Usecases and lottie files

--- network/src/main/java/com/aafaq/network/domain/utils/NetworkConstants.kt
+++ network/src/main/java/com/aafaq/network/domain/utils/NetworkConstants.kt
@@ -20,26 +20,23 @@ sealed class NetworkConstants {
             const val GET_PRAYER_TIMING_BY_CITY = "/timingsByCity/{date}"
         }
 
-        /**
-         * TODO - add methods here
-         * */
-        enum class Methods(name: String, numberValue: Int){
-            JAFARI("Shia Ithna-Ansari", 0),
-            KARACHI("University of Islamic Sciences, Karachi", 1),
-            NORTH_AMERICA("Islamic Society of North America", 2),
-            MWL("Muslim World League", 3),
-            MAKKAH("Umm Al-Qura University, Makkah", 4),
-            EGYPT("Egyptian General Authority of Survey", 5),
-            TEHRAN("Institute of Geophysics, University of Tehran", 6),
-            GULF_REGION("Gulf Region", 7),
-            KUWAIT("Kuwait", 8),
-            QATAR("Qatar", 9),
-            SINGAPORE("Majlis Ugama Islam Singapura, Singapore", 10),
-            FRANCE("Union Organization islamic de France", 11),
-            TURKEY("Diyanet İşleri Başkanlığı, Turkey", 12),
-            RUSSIA("Spiritual Administration of Muslims of Russia", 13),
-            MOONSIGHTING("Moonsighting Committee Worldwide", 14),
-            DUBAI("Dubai", 15),
+        enum class Methods(val methodName: String, val numberValue: Double){
+            JAFARI("Shia Ithna-Ansari", 0.0),
+            KARACHI("University of Islamic Sciences, Karachi", 1.0),
+            NORTH_AMERICA("Islamic Society of North America", 2.0),
+            MWL("Muslim World League", 3.0),
+            MAKKAH("Umm Al-Qura University, Makkah", 4.0),
+            EGYPT("Egyptian General Authority of Survey", 5.0),
+            TEHRAN("Institute of Geophysics, University of Tehran", 6.0),
+            GULF_REGION("Gulf Region", 7.0),
+            KUWAIT("Kuwait", 8.0),
+            QATAR("Qatar", 9.0),
+            SINGAPORE("Majlis Ugama Islam Singapura, Singapore", 10.0),
+            FRANCE("Union Organization islamic de France", 11.0),
+            TURKEY("Diyanet İşleri Başkanlığı, Turkey", 12.0),
+            RUSSIA("Spiritual Administration of Muslims of Russia", 13.0),
+            MOONSIGHTING("Moonsighting Committee Worldwide", 14.0),
+            DUBAI("Dubai", 15.0),
         }
     }
 }
